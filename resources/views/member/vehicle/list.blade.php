b39b1c362f373cb9979c4782b18ca97603b19dde
tien dat <toidaypk96@gmail.com>
2019-11-30 08:03:10 +0700

    feat:

--- resources/views/member/vehicle/list.blade.php
+++ resources/views/member/vehicle/list.blade.php
@@ -37,7 +37,7 @@
         <div class="col-12">
           <div class="card">
             <div class="card-header">
-              <h3 class="card-title">Danh sách biên nhận</h3>
+              <h3 class="card-title">Danh sách phương tiện</h3>
             </div>
             <!-- /.card-header -->
             <div class="card-body">
